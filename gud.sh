7b18838884125074cbb212ea81a511a0ac1333a5
Tom Schouten <tom@zwizwa.be>
2013-07-19 12:40:39 -0400

    gud.sh

--- gud.sh
+++ gud.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # FIXME: specific setup [tom]
 HERE=$(dirname $0)
-exec ~/sam7/gdb -i=mi $HERE/usb-device-ccid-project/bin/usb-device-ccid-project-simtrace-at91sam7s128-flash_dfu.elf
+PROJ=usb-device-ccid-project
+PROJ=simtrace-phone-project
+BOARD=simtrace-at91sam7s128-flash_dfu
+ELF=$HERE/$PROJ/bin/$PROJ-$BOARD.elf
+exec ~/sam7/gdb -i=mi $ELF
+
