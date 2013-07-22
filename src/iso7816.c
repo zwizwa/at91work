71704c5f3215150ed3852bf2abb03d4bf12953c6
Tom Schouten <tom@zwizwa.be>
2013-07-22 16:42:51 -0400

    PTS received

--- src/iso7816.c
+++ src/iso7816.c
@@ -49,32 +49,51 @@ enum {
 // phone side usart.
 static AT91S_USART * const phone_usart = AT91C_BASE_US1;
 
+uint32_t phone_status(void) {
+    /* Check */
+    uint32_t mask = AT91C_US_OVRE
+        | AT91C_US_FRAME
+        | AT91C_US_PARE
+        | AT91C_US_TIMEOUT
+        | AT91C_US_NACK
+        | (1<<10);
+    uint32_t csr = phone_usart->US_CSR;
+    uint32_t status = mask & csr;
+    if (status != 0) {
+        TRACE_DEBUG("csr: %08x %08x\n\r", csr, status);
+        phone_usart->US_CR = AT91C_US_RSTSTA | AT91C_US_RSTNACK;
+    }
+    return status;
+}
+
 uint32_t phone_tx(uint8_t c) {
+
     /* Switch RX/TX state if necessary. */
     if (phone_state != PHONE_TX) {
         phone_usart->US_CR = AT91C_US_RSTSTA | AT91C_US_RSTIT | AT91C_US_RSTNACK;
-        phone_usart->US_CR = AT91C_US_TXEN;
+        // phone_usart->US_CR = AT91C_US_TXEN;
         phone_state = PHONE_TX;
         TRACE_DEBUG("PHONE_TX\n\r");
     }
     /* Flush */
+    int timeout = 5000; //FIXME
     while((phone_usart->US_CSR & AT91C_US_TXRDY) == 0) {
+        if (!timeout--) {
+            TRACE_DEBUG("phone_tx timeout\n\r");
+            timeout = 5000;
+        }
         // TRACE_DEBUG("!TXRDY\n\r");
     }
-    TRACE_DEBUG("TXRDY\n\r");
+    // TRACE_DEBUG("TXRDY\n\r");
     /* Send */
     phone_usart->US_THR = c;
-    /* Check */
-    uint32_t status = phone_usart->US_CSR
-        &(AT91C_US_OVRE|AT91C_US_FRAME|
-          AT91C_US_PARE|AT91C_US_TIMEOUT|AT91C_US_NACK|
-          (1<<10));
-    if (status != 0) {
-        TRACE_DEBUG("E:0x%X\n\r",  phone_usart->US_CSR);
-        TRACE_DEBUG("Nb:0x%X\n\r", phone_usart->US_NER);
-        phone_usart->US_CR =  AT91C_US_RSTSTA;
-    }
-    return status;
+    return phone_status();
+}
+
+uint32_t phone_txn(uint8_t *c, int n) {
+    uint32_t rv = 0;
+    while(n--) rv = phone_tx(*c++);
+    return rv;
 }
 
 uint32_t phone_rx(uint8_t *c) {
@@ -84,7 +103,18 @@ uint32_t phone_rx(uint8_t *c) {
         phone_usart->US_CR = AT91C_US_RSTSTA | AT91C_US_RSTIT | AT91C_US_RSTNACK;
         phone_state = PHONE_RX;
     }
-    return 0;
+
+    /* Wait */
+    int timeout = 5000; //FIXME
+    while( ((phone_usart->US_CSR & AT91C_US_RXRDY) == 0) ) {
+        if (!timeout--) {
+            TRACE_DEBUG("phone_rx timeout\n\r");
+            timeout = 5000;
+        }
+    }
+    /* Read */
+    *c = ((phone_usart->US_RHR) & 0xFF);
+    return phone_status();
 }
 
 void phone_init(void) {
@@ -153,6 +183,7 @@ void phone_init(void) {
 
     uint32_t prev = 0;
     int cycles = 0;
+
     while(0) {
         // uint32_t mask = RST_PHONE | VCC_PHONE;
 
@@ -166,11 +197,112 @@ void phone_init(void) {
         cycles++;
     }
 
+
     while(1) {
-        uint8_t c = 0x55;
-        TRACE_DEBUG("sending %c\n\r", c);
-        phone_tx(c);
+        uint32_t mask =  RST_PHONE | VCC_PHONE;
+        while(mask != (AT91C_BASE_PIOA->PIO_PDSR & mask));
+        TRACE_DEBUG("ATR\n\r");
+        uint8_t atr[] = {0x3B, 0x98, 0x96, 0x00,
+                         0x93, 0x94, 0x03, 0x08,
+                         0x05, 0x03, 0x03, 0x03};
+        phone_txn(atr, sizeof(atr));
+
+        uint8_t c;
+        phone_rx(&c); TRACE_DEBUG("rx %02x\n\r", c);
+        phone_rx(&c); TRACE_DEBUG("rx %02x\n\r", c);
+        phone_rx(&c); TRACE_DEBUG("rx %02x\n\r", c);
+
+        mask = RST_PHONE;
+        while(mask == (AT91C_BASE_PIOA->PIO_PDSR & mask));
+        TRACE_DEBUG("OFF\n\r");
     }
 }
 
 
+#if 0
+
+/* Phone-side.  This takes care of:
+
+ - power/reset monitoring
+ - sequencing ATR / PTS / PTS ack
+ - invoking APDU callback
+
+*/
+
+
+// same as sniffer
+enum iso7816_3_state {
+    PHONE_S_RESET,
+    PHONE_S_WAIT_ATR,
+    PHONE_S_TX_ATR,
+    PHONE_S_RX_PTS_0,
+    PHONE_S_TX_PTS_ACK,
+};
+
+ struct phone_state {
+     enum iso7816_3_state state;
+     uint32_t time;
+     uint8_t *buf;
+     uint32_t buf_size;
+
+     uint8_t *atr;
+     uint32_t atr_size;
+};
+
+/* Environment access is abstracted to facilitate testing.
+   This is a deeply embedded target: compile-time binding is sufficient. */
+static inline int phone_rst(void)    { return AT91C_BASE_PIOA->PIO_PDSR & RST_PHONE; }
+static inline int phone_vcc(void)    { return AT91C_BASE_PIOA->PIO_PDSR & VCC_PHONE; }
+static inline int phone_rdy_tx(void) { return phone_usart->US_CSR & AT91C_US_TXRDY; }
+
+struct sm_sched {
+    /* Read-only */
+    uint32_t time;    // current time
+
+    /* Write only */
+    uint32_t delay;   // allow for time delay
+};
+
+/* Straightforward state machine. Machine-independent. */
+void phone_update(struct sm_sched *sched,
+                  struct phone_state *s) {
+    uint32_t rv = 0;
+
+    if (!(phone_rst() && phone_vcc())) {
+        s->state = PHONE_S_RESET;
+        return;
+    }
+
+    switch(s->state) {
+    case PHONE_S_RESET:
+        if (phone_rst() && phone_vcc()) {
+            s->state = ISO7816_S_WAIT_ATR;
+        }
+        break;
+    case PHONE_S_WAIT_ATR:
+        if (s->time >= sched->time) {
+            s->state = ISO7816_S_IN_ATR;
+            s->buf = s->atr;
+            s->buf_size = s->atr_size;
+        }
+        break;
+    case PHONE_S_TX_ATR:
+        if (!phone_bus_rdy_tx()) break;
+        if (0 != (rv = phone_tx(*(s->buf++)))) {
+            // FIXME: ???
+        }
+        s->buf_size--;
+        if (!s->buf_size) {
+            s->state = PHONE_S_RX_PTS;
+        }
+        break;
+    case PHONE_S_RX_PTS_0:
+        if (!phone_bus_rdy_rx()) break;
+        phone_rx(); // FIXME
+        break;
+    }
+
+}
+
+
+#endif
