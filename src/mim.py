c29c1cae54380e24b4d9bd9e3df116b215d97d32
Tom Schouten <tom@zwizwa.be>
2013-09-04 13:50:59 -0400

    factor mim.py into classes

--- src/mim.py
+++ src/mim.py
@@ -2,14 +2,16 @@
 
 # Bare-bones MIM - Delegate phone APDU requests to pyscard.
 #
-# The iso7816_slave firmware on the SIMtrace presents an ACM device.
-# Connect it to this script, e.g. using:
-#   socat /dev/ttyACM0 EXEC:./mim.py
-# 
 
-# force_SIM = True
 
-force_SIM = False
+# Structured as:
+
+# - apdu_fw class: connects to abstract apdu handler and to phone through SIMtrace
+# - apdu handler: supports interface:
+#    - apdu()
+#    - reset()
+
+
 
 
 import sys
@@ -25,32 +27,7 @@ import hextools
 log = sys.stderr.write
 
 
-# APDUs go over USB vendor control requests
 import usb
-def usb_find(idVendor, idProduct):
-    busses = usb.busses()
-    for bus in busses:
-        devices = bus.devices
-        for dev in devices:
-            if ((dev.idVendor  == idVendor) and
-                (dev.idProduct == idProduct)):
-                return dev
-    return 'wrong'
-dev = usb_find(idVendor=0x03eb, idProduct=0x6119) # CDC
-# dev = find(idVendor=0x03eb, idProduct=0x6129)  # CCID
-dh  = dev.open()
-def usb_ctrl_OUT(req, buf):
-    rv=dh.controlMsg(0x40,
-                     request=req,    # R-APDU
-                     buffer=buf,
-                     timeout=500)
-    return rv
-
-def usb_ctrl_IN(req):
-    return dh.controlMsg(0xC0,
-                         request=req,
-                         buffer=512,
-                         timeout=500)
 
 # SIMtrace slave commands and events, see iso7816_slave.h 
 CMD_SET_ATR  = 0
@@ -62,51 +39,30 @@ CMD_R_APDU   = 4
 EVT_RESET    = 2
 EVT_C_APDU   = 4
 
-
-def c_apdu():
-    while True:
-        msg = []
-        while (not len(msg)):
-            msg = usb_ctrl_IN(CMD_POLL)
-        evt = msg[0]
-
-        if (evt == EVT_C_APDU):
-            data = msg[4:]
-            return data
-
-        if (evt == EVT_RESET):
-            log("RESET CARD\n")
-            global c
-            c.disconnect()
-            c = card_connect()
-        else:
-            log("unknown event: %s\n" % hextools.bytes2hex(msg))
-
-def r_apdu(msg):
-    usb_ctrl_OUT(CMD_R_APDU, msg)
-
-def command(tag, payload=[]):  # dummy byte
-    log("CMD %d %s\n" % (tag, hextools.bytes2hex(payload)))
-    usb_ctrl_OUT(tag, payload)
+def usb_find(idVendor, idProduct):
+    busses = usb.busses()
+    for bus in busses:
+        devices = bus.devices
+        for dev in devices:
+            if ((dev.idVendor  == idVendor) and
+                (dev.idProduct == idProduct)):
+                return dev
+    return 'wrong'
 
 
+# see iso7816_slave.h
+# AT91 is little endian.
+def u32(val):
+    return [val & 0xFF,
+            (val >> 8) & 0xFF,
+            (val >> 16) & 0xFF,
+            (val >> 24) & 0xFF];
 
-
-# connect to card
-def card_connect():
-    r = smartcard.System.readers()
-    # print r # => ['SCM Microsystems Inc. SCR 331 [CCID Interface] (21121250210402) 00 00']
-    c = r[0].createConnection()
-    c.connect()
-    return c
-
-c = card_connect()
-
-
-
-
-
-# log("ATR = {%s}\n" % "".join(map(lambda v: "0x%02x, " % v, c.getATR())))
+def pack(reply,sw1,sw2):
+    p = list(reply)
+    p.append(sw1)
+    p.append(sw2)
+    return p
 
 # FIXME: remove this - use wireshark gsmtap instead.
 def pretty_apdu(msg):
@@ -132,77 +88,143 @@ def pretty_apdu(msg):
   except:
     pass
 
-def pack(reply,sw1,sw2):
-    p = list(reply)
-    p.append(sw1)
-    p.append(sw2)
-    return p
+
+class apdu_fw:
+    def __init__(self, handler, idVendor=0x03eb, idProduct=0x6119):
+        self.handler = handler
+        self.dev =  usb_find(idVendor=0x03eb, idProduct=0x6119) # CDC
+        # dev = find(idVendor=0x03eb, idProduct=0x6129)  # CCID
+        self.dh = self.dev.open()
+
+    def usb_ctrl_OUT(self, req, buf):
+        return self.dh.controlMsg(0x40,
+                                  request=req,    # R-APDU
+                                  buffer=buf,
+                                  timeout=500)
+    def usb_ctrl_IN(self, req):
+        return self.dh.controlMsg(0xC0,
+                                  request=req,
+                                  buffer=512,
+                                  timeout=500)
+    def log(self, msg):
+        sys.stderr.write(msg)
+
+    # FIXME: make this a proper event-handler loop
+    def c_apdu(self):
+        while True:
+            msg = []
+            while (not len(msg)):
+                msg = self.usb_ctrl_IN(CMD_POLL)
+
+            evt = msg[0]
+            if (evt == EVT_C_APDU):
+                data = msg[4:]
+                return data
+
+            if (evt == EVT_RESET):
+                log("RESET CARD\n")
+                self.handler.reset()
+            else:
+                log("unknown event: %s\n" % hextools.bytes2hex(msg))
+
+    def r_apdu(self, msg):
+        self.usb_ctrl_OUT(CMD_R_APDU, msg)
+
+    def command(self, tag, payload=[]):  # dummy byte
+        log("CMD %d %s\n" % (tag, hextools.bytes2hex(payload)))
+        self.usb_ctrl_OUT(tag, payload)
 
 
-# Delegate APDU handling to card.
-def default_delegate(msg):
-    data, sw1, sw2 = c.transmit( list(msg) )
-    return pack(data,sw1,sw2)
+
+    def tick(self):
+        c = self.c_apdu()
+        r = self.handler.apdu(c)
+        self.r_apdu(r)
+        gsmtap.log(c,r)
+        log("C-APDU:%s\n" % hextools.bytes2hex(c))
+        pretty_apdu(c)
+        log("R-APDU:%s\n" % hextools.bytes2hex(r))
 
 
-# Perform APDU request on card
-def apdu(msg, delegate = default_delegate):
+    def mainloop(self):
+        while 1:
+            self.tick()
 
-    # Force SIM protocol for USIM cards.
-    if (force_SIM):
-        if (msg[0] != 0xA0):
-            return [0x6E, 0x00]
+    def reboot(self):
+        adb = "/opt/xc/android/android-sdk-linux/platform-tools/adb"
+        log("adb shell reboot\n")
+        subprocess.call([adb, "shell", "reboot"])
 
-    if (msg[1] == 0x10): # TERMINAL_PROFILE
-        data, sw1, sw2 = c.transmit( list(msg) )
-        # Make phone poll for command
-        # return pack(data,0x91,0x20)
+    def run(self):
+        self.command(CMD_SET_ATR, self.handler.getATR())
+        self.command(CMD_SET_SKIP, u32(1))
+        self.command(CMD_HALT)
+
+        # start handling incoming phone C-APDUs
+        self.reboot()
+        self.mainloop()
+
+
+
+
+# Implements the handler interface
+class apdu_smartcard:
+    def __init__(self, index=0):
+        self.connect(index)
+        # self.force_SIM = True
+        self.force_SIM = False
+
+    def connect(self, index=0):
+        r = smartcard.System.readers()
+        # print r # => ['SCM Microsystems Inc. SCR 331 [CCID Interface] (21121250210402) 00 00']
+        self.c = r[index].createConnection()
+        self.c.connect()
+
+    def disconnect(self):
+        self.c.disconnect()
+    
+    def reset(self):
+        self.disconnect()
+        self.connect()
+
+    def getATR(self):
+        return self.c.getATR()
+
+    # Perform APDU request on card
+    def apdu(self, c_apdu):
+
+        # Force SIM protocol for USIM cards.
+        if (self.force_SIM):
+            if (c_apdu[0] != 0xA0):
+                return [0x6E, 0x00]
+
+        if (c_apdu[1] == 0x10): # TERMINAL_PROFILE
+            data, sw1, sw2 = self.c.transmit( list(c_apdu) )
+            # Make phone poll for command
+            # return pack(data,0x91,0x20)
+            return pack(data,sw1,sw2)
+
+        # Delegate to MIM.
+        (data,sw1,sw2) = self.c.transmit( list(c_apdu) )
         return pack(data,sw1,sw2)
 
-    #if (msg[1] == 0x2C): # UNBLOCK_PIN
-    #    #time.sleep(1)
-    #    return [0x63, 0xCA]
-
-    # Delegate to MIM.
-    return delegate( msg )
 
 
-# see iso7816_slave.h
-# AT91 is little endian.
-def u32(val):
-    return [val & 0xFF,
-            (val >> 8) & 0xFF,
-            (val >> 16) & 0xFF,
-            (val >> 24) & 0xFF];
+simcard = apdu_smartcard()
+mitm = apdu_fw(handler = simcard)
+
+mitm.run()
 
 
-def tick():
-    c = c_apdu()
-    r = apdu(c)
-    r_apdu(r)
-    gsmtap.log(c,r)
-    log("C-APDU:%s\n" % hextools.bytes2hex(c))
-    pretty_apdu(c)
-    log("R-APDU:%s\n" % hextools.bytes2hex(r))
+# log("ATR = {%s}\n" % "".join(map(lambda v: "0x%02x, " % v, c.getATR())))
 
 
-def mainloop():
-    while 1:
-        tick()
-
-command(CMD_SET_ATR, c.getATR())
-command(CMD_SET_SKIP, u32(1))
-command(CMD_HALT)
-
-# reboot android phone
-adb = "/opt/xc/android/android-sdk-linux/platform-tools/adb"
-def reboot():
-    subprocess.call([adb, "shell", "reboot"])
 
 
-# start handling incoming phone C-APDUs
-reboot()
-mainloop()
+
+
+
+
 
 
 
